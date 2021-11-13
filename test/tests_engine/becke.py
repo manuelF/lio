99ba84dbe3a3c935bb55ab3c2105942fd3782447
Manuel Ferreria <mferreria@gmail.com>
2021-11-14 00:16:00 +0100

    Tests: Apply python types, formatting and pep8 practices

--- test/tests_engine/becke.py
+++ test/tests_engine/becke.py
@@ -1,70 +1,73 @@
 #!/usr/bin/env python3
-import re
 import os
+import re
+from typing import List, Literal, TextIO
 
-def obtain_becke(file_in):
-   lista = []
-   for line in file_in.readlines():
-      m = re.match("\s+\d+\s+\d+\s+([0-9.-]+)",line)
-      if m:
-         lista.append(float(m.group(1)))
 
-      m = re.match("\s+Total Charge =\s+([0-9.-]+)",line)
-      if m:
-         lista.append(float(m.group(1)))
+def obtain_becke(file_in: TextIO) -> List[float]:
+    lista: List[float] = []
+    for line in file_in.readlines():
+        m = re.match(r"\s+\d+\s+\d+\s+([0-9.-]+)", line)
+        if m:
+            lista.append(float(m.group(1)))
 
-   return lista
+        m = re.match(r"\s+Total Charge =\s+([0-9.-]+)", line)
+        if m:
+            lista.append(float(m.group(1)))
 
-def error(mull,mull_ok):
-   dim1 = len(mull)
-   dim2 = len(mull_ok)
-   scr = 0
-   if dim1 != dim2:
-      print("There are diffenrent becke charges in outputs.")
-      return -1
+    return lista
 
-   for num in range(dim1):
-      value = abs(mull[num] - mull_ok[num])
-      if value > 1e-2:
-         src = -1
-         print("Error in becke charges:")
-         print("Valor en becke",mull[num])
-         print("Valor en becke.ok",mull_ok[num])
 
-   return scr
+def error(mull: List[float], mull_ok: List[float]) -> Literal[0, -1]:
+    dim1 = len(mull)
+    dim2 = len(mull_ok)
+    scr = 0
+    if dim1 != dim2:
+        print("There are diffenrent becke charges in outputs.")
+        return -1
+
+    for num in range(dim1):
+        value = abs(mull[num] - mull_ok[num])
+        if value > 1e-2:
+            scr = -1
+            print("Error in becke charges:")
+            print("Valor en becke", mull[num])
+            print("Valor en becke.ok", mull_ok[num])
+
+    return scr
 
 
 def Check():
-   # Output
-   mull = []
-   is_file = os.path.isfile("becke")
-   if is_file == False:
-      print("The becke file is missing.")
-      return -1
+    # Output
+    mull = []
+    is_file = os.path.isfile("becke")
+    if not is_file:
+        print("The becke file is missing.")
+        return -1
 
-   f = open("becke","r")
-   mull = obtain_becke(f)
-   f.close()
-   if not mull:
-      print("Error in reading becke.")
-      return -1
+    f = open("becke", "r")
+    mull = obtain_becke(f)
+    f.close()
+    if not mull:
+        print("Error in reading becke.")
+        return -1
 
-   # Ideal Output
-   is_file = os.path.isfile("becke.ok")
-   if is_file == False:
-      print("The becke.ok file is missing.")
-      return -1
+    # Ideal Output
+    is_file = os.path.isfile("becke.ok")
+    if not is_file:
+        print("The becke.ok file is missing.")
+        return -1
 
-   f = open("becke.ok","r")
-   mullok = []
-   mullok = obtain_becke(f)
-   f.close()
-   if not mullok:
-      print("Error in reading becke.ok.")
-      return -1
+    f = open("becke.ok", "r")
+    mullok = []
+    mullok = obtain_becke(f)
+    f.close()
+    if not mullok:
+        print("Error in reading becke.ok.")
+        return -1
 
-   ok_output = error(mull,mullok)
-   if ok_output != 0:
-      print("Test Becke:      ERROR")
-   else:
-      print("Test Becke:      OK")
+    ok_output = error(mull, mullok)
+    if ok_output != 0:
+        print("Test Becke:      ERROR")
+    else:
+        print("Test Becke:      OK")
